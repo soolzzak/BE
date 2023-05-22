311716a8fe84ead35e4aa8109643b1d6f56ebd32
SangWon7242 <ivv11@naver.com>
2023-05-23 00:22:39 +0900

    fix:이미지 업로드 이름처리 및 파일제한 설정

--- src/main/java/com/example/zzan/global/exception/ExceptionEnum.java
+++ src/main/java/com/example/zzan/global/exception/ExceptionEnum.java
@@ -15,7 +15,9 @@ public enum ExceptionEnum {
     ROOM_NOT_FOUND(HttpStatus.NOT_FOUND.value(),  "방이 존재하지 않습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "아이디가 존재하지 않습니다."),
     USERS_DUPLICATION(HttpStatus.CONFLICT.value(), "중복된 이메일이 존재합니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버에러가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버에러가 발생했습니다."),
+    INVALID_FILE(HttpStatus.BAD_REQUEST.value(), "전달 값이 잘못 되었습니다");
+
 
     private final int status;
     private final String message;
