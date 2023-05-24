753c6199597b00442f81b7947ec4847bd3142fb3
SangWon7242 <ivv11@naver.com>
2023-05-24 14:45:32 +0900

    fix:팔로우 유효성 검사 업데이트

--- src/main/java/com/example/zzan/follow/repository/FollowRepository.java
+++ src/main/java/com/example/zzan/follow/repository/FollowRepository.java
@@ -1,8 +1,11 @@
 package com.example.zzan.follow.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.zzan.follow.entity.Follow;
 
 public interface FollowRepository extends JpaRepository<Follow,Long> {
+	Optional<Follow> findByFollowingUserEmailAndUserEmail(String followingUserEmail, String email);
 }
