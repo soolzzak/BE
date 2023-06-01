255ce02f4dcabb529a67ae14ed07322d39bff920
LEOJI <kjh11-07@hanmail.net>
2023-06-01 23:34:07 +0900

    Feat : mail 인증 api 추가

--- src/main/java/com/example/zzan/user/controller/UserController.java
+++ src/main/java/com/example/zzan/user/controller/UserController.java
@@ -6,25 +6,27 @@ import com.example.zzan.global.util.JwtUtil;
 import com.example.zzan.user.dto.UserLoginDto;
 import com.example.zzan.user.dto.UserRequestDto;
 import com.example.zzan.user.service.KakaoService;
+import com.example.zzan.user.service.MailService;
 import com.example.zzan.user.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
     private final UserService userService;
-
-
-
     private final KakaoService kakaoService;
+    private final MailService mailService;
+
     @GetMapping("/login/oauth2/code/kakao")
     public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
         // code: 카카오 서버로부터 받은 인가 코드
@@ -38,10 +40,17 @@ public class UserController {
         return "index";
     }
 
-
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@Valid @RequestBody UserRequestDto requestDto){
         return userService.signup(requestDto);
+    }
+
+    @PostMapping("/signup/mailConfirm")
+    @ResponseBody
+    public String mailConfirm(@RequestParam String email) throws Exception {
+        String code = mailService.sendSimpleMessage(email);
+        log.info("인증코드 : " + code);
+        return code;
     }
 
     @PostMapping("/login")
