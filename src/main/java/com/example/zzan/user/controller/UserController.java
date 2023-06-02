82598adbb9b88f01a7fdab65e6759c31e96dceb0
LEOJI <kjh11-07@hanmail.net>
2023-06-03 03:44:12 +0900

    Feat : controller에 @CrossOrigin 추가

--- src/main/java/com/example/zzan/user/controller/UserController.java
+++ src/main/java/com/example/zzan/user/controller/UserController.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
