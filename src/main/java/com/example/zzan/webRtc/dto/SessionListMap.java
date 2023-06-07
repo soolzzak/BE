64f572456b9024afde31c598143779d5d7e3448b
Jello92 <jay.ji92@gmail.com>
2023-06-08 01:26:07 +0900

    Fixes: roomLeave 위한 시그널 채널 수정

---
+++ src/main/java/com/example/zzan/webRtc/dto/SessionListMap.java
@@ -0,0 +1,26 @@
+package com.example.zzan.webRtc.dto;
+
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.web.socket.WebSocketSession;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SessionListMap {
+
+    private static SessionListMap sessionListMap = new SessionListMap();
+    private Map<WebSocketSession, Long> sessionMapToUserId = new LinkedHashMap<>();
+    private Map<WebSocketSession, Long> sessionMapToRoom = new LinkedHashMap<>();
+    private SessionListMap(){}
+
+    public static SessionListMap getInstance(){
+        return sessionListMap;
+    }
+
+}
