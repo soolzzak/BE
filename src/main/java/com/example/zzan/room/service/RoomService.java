fe771734eb7f4a02d00bc2585e9dfc8c52b73d3b
kimsangwoo0505 <ivv11@naver.com>
2023-06-16 04:10:19 +0900

    fix:룸이미지 수정부분 수정

--- src/main/java/com/example/zzan/room/service/RoomService.java
+++ src/main/java/com/example/zzan/room/service/RoomService.java
@@ -135,8 +135,7 @@ public class RoomService {
         if (roomImage == null) {
             roomImageUrl = s3Uploader.getRandomImage("Random");
         } else {
-            s3Uploader.removeNewFile(new File(room.getRoomImage()));
-            roomImageUrl = s3Uploader.upload(roomImage, "images");
+            roomImageUrl = s3Uploader.upload(roomImage, "mainImage");
         }
         room.setRoomImage(roomImageUrl);
         String roomTitle = roomRequestDto.getTitle();
