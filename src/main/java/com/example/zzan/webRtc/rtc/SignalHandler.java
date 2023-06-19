0e30a1800766ebf5ea01e1ff79fed146625be356
LEOJI <kjh11-07@hanmail.net>
2023-06-20 00:41:15 +0900

    Fix : host, guest 메세지가 가도록 수정

--- src/main/java/com/example/zzan/webRtc/rtc/SignalHandler.java
+++ src/main/java/com/example/zzan/webRtc/rtc/SignalHandler.java
@@ -312,7 +312,7 @@ public class SignalHandler extends TextWebSocketHandler {
 
                     Map<Long, WebSocketSession> youtubeViewers = rtcChatService.getUser(room);
                     for (Map.Entry<Long, WebSocketSession> client : youtubeViewers.entrySet()) {
-                        if (!client.getKey().equals(userId)) {
+//                        if (!client.getKey().equals(userId)) {
                             sendMessage(client.getValue(),
                                 new WebSocketMessage(
                                     userId,
@@ -321,7 +321,7 @@ public class SignalHandler extends TextWebSocketHandler {
                                     time,
                                     null,
                                     null));
-                        }
+//                        }
                     }
                     break;
 
