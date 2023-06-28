d6f17f34703bf67645588c1e3e89da876f1c9125
kimsangwoo0505 <ivv11@naver.com>
2023-06-28 12:00:05 +0900

    fix:http only수정

--- src/main/java/com/example/zzan/user/service/UserService.java
+++ src/main/java/com/example/zzan/user/service/UserService.java
@@ -151,17 +151,8 @@ public class UserService {
 
 
     private void setHeader(HttpServletResponse response, TokenDto tokenDto, String userEmail) {
-
-        String accessToken = tokenDto.getAccessToken();
-        String refreshToken = tokenDto.getRefreshToken();
-
-        if (accessToken.startsWith("Bearer ")) {
-            accessToken = accessToken.substring(7);
-        }
-
-        if (refreshToken.startsWith("Bearer ")) {
-            refreshToken = refreshToken.substring(7);
-        }
+        String accessToken = tokenDto.getAccessToken().replace(" ", "%20");
+        String refreshToken = tokenDto.getRefreshToken().replace(" ", "%20");
 
         Cookie accessTokenCookie = new Cookie(ACCESS_KEY, accessToken);
         accessTokenCookie.setHttpOnly(true);
@@ -177,6 +168,7 @@ public class UserService {
 
         response.addHeader("USER-EMAIL", userEmail);
     }
+
 
 
 
