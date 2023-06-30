2c16bc16775ad1f1a1bcb5558a2e47ce51892d18
kimsangwoo0505 <ivv11@naver.com>
2023-06-30 16:07:07 +0900

    fix:토큰 수정

--- src/main/java/com/example/zzan/global/jwt/JwtUtil.java
+++ src/main/java/com/example/zzan/global/jwt/JwtUtil.java
@@ -31,6 +31,10 @@ public class JwtUtil {
 
     @Value("${jwt.secret.key}")
     private String secretKey;
+
+    @Value("${jwt.refresh.key}")
+    private String refreshSecretKey;
+
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String ACCESS_KEY = "ACCESS_KEY";
     public static final String REFRESH_KEY = "REFRESH_KEY";
@@ -38,13 +42,13 @@ public class JwtUtil {
     public static final String BEARER_PREFIX = "Bearer ";
     private static final long ACCESS_TIME = 60 * 60 * 1000L;
     // private static final long ACCESS_TIME = 30 * 1000L;
-
     private static final long REFRESH_TIME = 7 * 24 * 60 * 60 * 1000L;
     private final UserDetailsServiceImpl userDetailsService;
     private final RefreshTokenRepository refreshTokenRepository;
 
     private Key key;
     private Key kakaoKey;
+    private Key refreshKey;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
     @PostConstruct
@@ -52,6 +56,8 @@ public class JwtUtil {
         byte[] bytes = Base64.getDecoder().decode(secretKey);
         key = Keys.hmacShaKeyFor(bytes);
         kakaoKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        byte[] refreshKeyBytes = Base64.getDecoder().decode(refreshSecretKey);
+        refreshKey = Keys.hmacShaKeyFor(refreshKeyBytes);
     }
 
     public TokenDto createAllToken(User user, UserRole role) {
@@ -70,6 +76,7 @@ public class JwtUtil {
     public String createToken(User user, UserRole role, String type) {
         Date date = new Date();
         long time = type.equals(ACCESS_KEY) ? ACCESS_TIME : REFRESH_TIME;
+        String key = type.equals(ACCESS_KEY) ? secretKey : refreshSecretKey;
         Map<String, Object> claim = new HashMap<>();
         claim.put("id",user.getId());
         claim.put("role",role);
@@ -78,7 +85,7 @@ public class JwtUtil {
         return BEARER_PREFIX
             + Jwts.builder()
             .setSubject(user.getEmail())
-            .signWith(signatureAlgorithm, secretKey)
+            .signWith(signatureAlgorithm, key)
             .claim(AUTHORIZATION_KEY, claim)
             .setIssuedAt(date)
             .setExpiration(new Date(date.getTime() + time))
@@ -121,16 +128,38 @@ public class JwtUtil {
         }
     }
 
+    public String validateRefreshtoken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(refreshKey).build().parseClaimsJws(token);
+            return null;
+        } catch (SecurityException | MalformedJwtException e) {
+            return ExceptionEnum.INVALID_JWT_SIGNATURE.getMessage();
+        } catch (ExpiredJwtException e) {
+            return ExceptionEnum.ACCESS_TOKEN_NOT_FOUND.getMessage();
+        } catch (UnsupportedJwtException e) {
+            return ExceptionEnum.UNSUPPORTED_JWT_TOKEN.getMessage();
+        } catch (IllegalArgumentException e) {
+            return ExceptionEnum.EMPTY_JWT_CLAIMS.getMessage();
+        }
+    }
+
+
+
     public String getUserInfoFromToken(String token) {
         return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
     }
+
+    public String getUserInfoFromRefreshtoken(String token) {
+        return Jwts.parserBuilder().setSigningKey(refreshKey).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
     public Authentication createAuthentication(String userId) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 
     public Boolean refreshTokenValidation(String token) {
-        String validationError = validateToken(token);
+        String validationError = validateRefreshtoken(token);
         log.info("Adding user {} to blacklist for user {}", validationError, token);
         if (validationError != null){
             return false;
